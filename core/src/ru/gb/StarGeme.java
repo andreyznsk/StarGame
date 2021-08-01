8323f44010145547c0bd5b11993c00982946f8de
andreyznsk <andreyznsk@gmail.com>
2021-08-01 16:01:13 +0700

    1 Добавил фоновую картинку

--- core/src/ru/gb/StarGeme.java
+++ core/src/ru/gb/StarGeme.java
@@ -12,14 +12,14 @@ public class StarGeme extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("Logo.jpg");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0.4f, 0.24f, 0.51f, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img,0,0);
 		batch.end();
 	}
 	
