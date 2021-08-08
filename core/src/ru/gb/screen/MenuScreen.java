9c1a88f054790771939ac89d6223448bb3992b8e
andreyznsk <andreyznsk@gmail.com>
2021-08-08 12:32:41 +0700

    Исправил неточности по уроку 2

--- core/src/ru/gb/screen/MenuScreen.java
+++ core/src/ru/gb/screen/MenuScreen.java
@@ -8,10 +8,12 @@ import ru.gb.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
 
+    private static final float V_LEN = 0.05f;
+
     private Texture img;
     private Texture backGround;
     private Vector2 pos;
-    private Vector2 nor;
+    private Vector2 v;
     private Vector2 targetPos;
 
     @Override
@@ -20,7 +22,7 @@ public class MenuScreen extends BaseScreen {
         img = new Texture("G03.jpg");
         backGround = new Texture("Logo.jpg");
         pos = new Vector2();
-        nor = new Vector2();
+        v = new Vector2();
         targetPos = new Vector2();
     }
 
@@ -31,8 +33,8 @@ public class MenuScreen extends BaseScreen {
         batch.draw(backGround, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.draw(img, pos.x, pos.y);
         batch.end();
-        if (!pos.epsilonEquals(targetPos,2f)) {
-            pos.add(nor);
+        if (targetPos.dst(pos) > V_LEN) {
+            pos.add(v);
         } else {
             pos.set(targetPos);
         }
@@ -48,11 +50,8 @@ public class MenuScreen extends BaseScreen {
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         targetPos.set(screenX, Gdx.graphics.getHeight() - screenY);
-        System.out.println("targetPos: " + targetPos);
-        System.out.println("Pos: " + pos);
-        Vector2 dif = targetPos.sub(pos);
-        nor = dif.cpy().nor();
-        System.out.println("nor: " + nor);
+        v.set(targetPos.cpy().sub(pos)).scl(V_LEN);
+        System.out.println(v);
         return false;
     }
 
